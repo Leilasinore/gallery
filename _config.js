25d47d7dbfab36ecbbb61563d98b9934fd75ebbd
Leila Sinore <leilasinore99@gmail.com>
2025-03-29 21:26:18 +0300

    corrected db user

--- _config.js
+++ _config.js
@@ -2,8 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://leila:Entito1963@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://leila:Entito1963@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://leila:Entito1963@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production:
+    'mongodb+srv://leila:Entito1963@gallery.qmbelyw.mongodb.net/darkroom?retryWrites=true&w=majority&appName=gallery',
+  development:
+    'mongodb+srv://leila:Entito1963@gallery.qmbelyw.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=gallery',
+  test: 'mongodb+srv://leila:Entito1963@gallery.qmbelyw.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=gallery',
+};
 module.exports = config;
