accee391b8de42476c2cbaf03fa516f31da5ed02
Leila Sinore <leilasinore99@gmail.com>
2025-03-29 22:15:53 +0300

    testing db connection

--- _config.js
+++ _config.js
@@ -3,9 +3,9 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
   production:
-    'mongodb+srv://leila:Entito1963@gallery.qmbelyw.mongodb.net/darkroom?retryWrites=true&w=majority&appName=gallery',
+    'mongodb+srv://leila:2CBajYvm3dTT6tsi@gallery.qmbelyw.mongodb.net/darkroom?retryWrites=true&w=majority&appName=gallery',
   development:
-    'mongodb+srv://leila:Entito1963@gallery.qmbelyw.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=gallery',
-  test: 'mongodb+srv://leila:Entito1963@gallery.qmbelyw.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=gallery',
+    'mongodb+srv://leila:2CBajYvm3dTT6tsi@gallery.qmbelyw.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=gallery',
+  test: 'mongodb+srv://leila:2CBajYvm3dTT6tsi@gallery.qmbelyw.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=gallery',
 };
 module.exports = config;
